abcc7ba538d3471672a0087b09316fcd2ccb615c
Anna <jerofejeva.anna@yahoo.com>
2015-01-01 20:04:08 +0000

    Created Card class

---
+++ PokerHands/PokerHands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PokerHands
+{
+    public class Card
+    {
+        public string suit { get; set;
+        }
+        public string value { get; set;
+        }
+
+        public Card(string suit, string value)
+        {
+            this.suit = suit;
+            this.value = value;
+        }
+
+        public static void Main()
+        { 
+        }
+    }
+}
---
+++ UnitTestPokerhands/UnitTestsPokerHands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokerHands;
+
+namespace UnitTestsPokerHands
+{
+    [TestClass]
+    public class UnitTestsCard
+    {
+        [TestMethod]
+        public void CardIsInitializedWithSuitAndValue()
+        {
+            Card card = new Card("H", "8");
+            Assert.AreEqual("H", card.suit);
+            Assert.AreEqual("0", card.value);
+        }
+    }
+}
