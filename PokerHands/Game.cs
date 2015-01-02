738a5ab78d9d505123885b6f73023a368138bf44
Anna <jerofejeva.anna@yahoo.com>
2015-01-02 18:20:58 +0000

    Added game class

---
+++ PokerHands/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace PokerHands
+{
+    public class Game
+    {
+        public List<Player> players {get; set;
+        }
+
+        public Game()
+        {
+            this.players = new List<Player>();
+        }
+    }
+}
---
+++ UnitTestPokerhands/UnitTestsGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokerHands;
+using System.Collections.Generic;
+
+namespace UnitTestPokerhands
+{
+    [TestClass]
+    public class UnitTestsGame
+    {
+        [TestMethod]
+        public void GameIsInitializedWithAnEmptyListOfPlayers()
+        {
+            Game poker = new Game();
+            Assert.AreEqual(0, poker.players.Count);
+        }
+    }
+}
